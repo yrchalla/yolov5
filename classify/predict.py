0804dd3d8d28db1f75aef2284830a0d65e0a5886
yrchalla <111409506+yrchalla@users.noreply.github.com>
2023-06-17 10:19:35 +0530

    Update predict.py
    Signed-off-by: yrchalla <111409506+yrchalla@users.noreply.github.com>

--- classify/predict.py
+++ classify/predict.py
@@ -57,7 +57,7 @@ def run(
         weights=ROOT / 'yolov5s-cls.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(224, 224),  # inference size (height, width)
+        imgsz=(512, 512),  # inference size (height, width)
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
         save_txt=False,  # save results to *.txt
